7a294174969404e836edb25864173a5bd06a5064
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-03-05 14:03:37 +0530

    Update memberstic.py

--- Emli/modules/memberstic.py
+++ Emli/modules/memberstic.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from Emli import app,eor
+from Emli import pbot as app,eor
 from Emli.core.decorators.errors import capture_err
 from pyrogram import filters
 
