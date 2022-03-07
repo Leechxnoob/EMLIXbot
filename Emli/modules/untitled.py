03fe636ef9c565d6db7a3092b56714222494b524
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-03-07 13:45:40 +0530

    Add files via upload

---
+++ Emli/modules/untitled.py
@@ -0,0 +1,29 @@
+__help__ = """
+
+**Member Sticker**
+
+Automatically Thank Members of a Group With sticker 
+
+**Admin Commands:**
+ × /memberstic `[on|off]`
+ 
+Note : Member count 3,5,7 etc...
+    
+ **Anti-Channel**
+ your groups to stop anonymous channels sending messages into your chats.
+**Type of messages**
+        - document
+        - photo
+        - sticker
+        - animation
+        - video
+        - text
+        
+**Admin Commands:**
+ × /antichannel [on / off] - Anti- channel  function 
+**Note** : If linked_channel  send any containing characters in this type when on  function no del    
+
+    
+"""
+
+__mod_name__ = "Advtools"
