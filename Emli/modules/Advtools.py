8737eb047554c739b8869fd9078855bbae377ae8
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-03-07 13:49:30 +0530

    Update and rename untitled.py to Advtools.py

--- Emli/modules/Advtools.py
+++ Emli/modules/Advtools.py
@@ -1,6 +1,6 @@
 __help__ = """
 
-**Member Sticker**
+★ **Member Sticker**
 
 Automatically Thank Members of a Group With sticker 
 
@@ -9,7 +9,7 @@ Automatically Thank Members of a Group With sticker
  
 Note : Member count 3,5,7 etc...
     
- **Anti-Channel**
+ ★ **Anti-Channel**
  your groups to stop anonymous channels sending messages into your chats.
 **Type of messages**
         - document
@@ -22,7 +22,13 @@ Note : Member count 3,5,7 etc...
 **Admin Commands:**
  × /antichannel [on / off] - Anti- channel  function 
 **Note** : If linked_channel  send any containing characters in this type when on  function no del    
+★ **Logo**
 
+×  /logo [TEXT]: Create a logo 
+×  /logohq [TEXT]: Create a HQ logo 
+×  /write [TEXT] : hand writer
+×  /wall [TEXT] : search wallpapers
+×  /rlogo [TEXT] : create random logo
     
 """
 
