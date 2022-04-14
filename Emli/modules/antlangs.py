4279c1e92a2406a90870974f202447c068cae694
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-04-14 08:32:39 +0530

    Update and rename Antilang.py to antlangs.py

--- Emli/modules/antlangs.py
+++ Emli/modules/antlangs.py
@@ -388,23 +388,4 @@ Usage: /antitamil `[on | off]`
     await sax.edit(f"✅ **Successfully** `{'Enabled' if lower_args=='on' else 'Disabled'}` **Tamil Detection Guard**")
 
 
-__MODULE__ = " F-Sub"
-__HELP__ = """
-
-**ForceSubscribe | Channel manager:**
-- Rose can mute members who are not subscribed your channel until they subscribe
-- When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them.
-
-**Setup**
-1) First of all add me in the group as admin with ban users permission and in the channel as admin.
-Note: Only creator of the group can setup me and i will not allow force subscribe again if not done so.
-
-
-**Commmands**
-- /forcesubscribe - To get the current settings.
-- /forcesubscribe no/off/disable - To turn of ForceSubscribe.
-- /forcesubscribe {channel username} - To turn on and setup the channel.
-- /forcesubscribe clear - To unmute all members who muted by me.
-
-Note: /forcesub is an alias of /forcesubscribe
-"""
+__MODULE__ = " antlangs"
