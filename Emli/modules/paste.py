3913755927dc0c68414c9d3d8e98e2b5e0063f72
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-05-17 13:41:17 +0530

    Update paste.py

--- Emli/modules/paste.py
+++ Emli/modules/paste.py
@@ -6,12 +6,12 @@ import aiofiles
 from pykeyboard import InlineKeyboard
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton
-
+from Emli.core.keyboard import ikb
 from Emli import aiohttpsession as session
-from Emli import pbot as app
+from Emli import pbot as app eor
 from Emli.utils.errors import capture_err
 from Emli.utils.pastebin import paste
-
+from Emli.utils.pastebin import bpaste
 __mod_name__ = "Paste"
 
 pattern = re.compile(
@@ -70,3 +70,53 @@ async def paste_func(_, message):
         except Exception:
             pass
     return await m.edit(link)
+
+
+@app.on_message(filters.command("bpaste") & ~filters.edited)
+@capture_err
+async def paste_func(_, message: Message):
+    if not message.reply_to_message:
+        return await eor(message, text="Reply To A Message With /paste")
+    r = message.reply_to_message
+
+    if not r.text and not r.document:
+        return await eor(
+            message, text="Only text and documents are supported."
+        )
+
+    m = await eor(message, text="Pasting...")
+
+    if r.text:
+        content = str(r.text)
+    elif r.document:
+        if r.document.file_size > 40000:
+            return await m.edit("You can only paste files smaller than 40KB.")
+
+        if not pattern.search(r.document.mime_type):
+            return await m.edit("Only text files can be pasted.")
+
+        doc = await message.reply_to_message.download()
+
+        async with aiofiles.open(doc, mode="r") as f:
+            content = await f.read()
+
+        os.remove(doc)
+
+    link = await paste(content)
+    kb = ikb({"Paste Link": link})
+    try:
+        if m.from_user.is_bot:
+            await message.reply_photo(
+                photo=link,
+                quote=False,
+                reply_markup=kb,
+            )
+        else:
+            await message.reply_photo(
+                photo=link,
+                quote=False,
+                caption=f"**Paste Link:** [Here]({link})",
+            )
+        await m.delete()
+    except Exception:
+        await m.edit("Here's your paste", reply_markup=kb)
