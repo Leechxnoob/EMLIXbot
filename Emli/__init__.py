e49db1a0f3e69e50ce3c86036446c1d3c3beecaa
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-05-22 14:47:06 +0530

    Update __init__.py

--- Emli/__init__.py
+++ Emli/__init__.py
@@ -83,6 +83,7 @@ if ENV:
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     DB_URL = os.environ.get("DATABASE_URL")
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
     REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
     MONGO_DB_URI = os.environ.get("MONGO_DB_URI", None)
     ARQ_API = os.environ.get("ARQ_API", None)
