bfdd014eece0ec38fbd55e9afe6401c6e680f13f
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-06-19 17:31:35 +0530

    Update eval.py

--- Emli/modules/eval.py
+++ Emli/modules/eval.py
@@ -1,146 +1,191 @@
-import io
 import os
+import re
+import subprocess
 import sys
-import textwrap
-import json
-import asyncio
 import traceback
-from telethon import TelegramClient, events
-from Emli import*
-from Emli import telethn,  TEMP_DOWNLOAD_DIRECTORY, SUPPORT_CHAT 
-from Emli import pbot
+from inspect import getfullargspec
+from io import StringIO
+from time import time
 
-class Database:
-    def __init__(self):
-        self.db = {}
-        if not os.path.exists("db.json"):
-            with open("db.json", "w") as f:
-                f.write("{}")
-        else:
-            with open("db.json", "r") as f:
-                self.db = json.load(f)
+from pyrogram import filters
+from pyrogram.types import (InlineKeyboardButton,
+                            InlineKeyboardMarkup, Message)
 
-    def get(self, key):
-        return self.db.get(key, None)
-
-    def set(self, key, value):
-        self.db[key] = value
-        with open("db.json", "w") as f:
-            json.dump(self.db, f)
-
-    def isOwner(self, user):
-        return user == self.get("OWNER")
-
-    def isAuth(self, user):
-        return user in self.get("AUTH") or self.isOwner(user)
+from Emli import pbot as app
 
 
-
-async def aexec(code, event):
+async def aexec(code, client, message):
     exec(
-        'async def __aexec(event, client): '
-        + '\n reply = await event.get_reply_message()'
-        + '\n chat = event.chat_id'
-        + ''.join(f'\n {l}' for l in code.split('\n'))
+        "async def __aexec(client, message): "
+        + "".join(f"\n {a}" for a in code.split("\n"))
     )
-    return await locals()['__aexec'](event, event.client)
+    return await locals()["__aexec"](client, message)
 
-def getEnv(key):
-    value = os.environ.get(key, None)
-    if value is None:
-        raise KeyError(f"{key} is not set")
-        sys.exit(1)
-    return value
 
-db = Database()
-db.set("OWNER", int(getEnv("OWNER_ID")))
-auth_ = getEnv("AUTH_USERS").split(" ")
-db.set("AUTH", [int(i) for i in auth_])
+async def edit_or_reply(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    await func(**{k: v for k, v in kwargs.items() if k in spec})
 
-@telethn.on(events.NewMessage(pattern="^/auth$"))
-async def auth_actions(event):
-    if not db.isOwner(event.sender_id):
-        return
-    auth = list(db.get("AUTH"))
-    reply = await event.get_reply_message()
-    if reply is None:
-        ulist = "\n".join(f"`{i}`" for i in auth)
-        await event.reply("Auth list:\n" + ulist)
-        return
-    if reply.sender_id in auth:
-        auth.remove(reply.sender_id)
-        db.set("AUTH", auth)
-        await event.reply(f"Removed `{reply.sender_id}` from auth list")
-    else:
-        auth.append(reply.sender_id)
-        db.set("AUTH", auth)
-        await event.reply(f"Added `{reply.sender_id}` to auth list")
 
-@telethn.on(events.NewMessage(pattern="^/restart$"))
-async def restart_action(event):
-    if not db.isOwner(event.sender_id):
-        return
-    await event.reply("Restarting...")
-    os.execl(sys.executable, sys.executable, *sys.argv)
-    sys.exit(0)
-
-@telethn.on(events.NewMessage(pattern="^/(bash|sh)"))
-async def bash_action(event):
-    if not db.isAuth(event.sender_id):
-        return
-    cmd = event.raw_text.split(" ", 1)
-    if len(cmd) == 1:
-        await event.reply("Usage: `/sh <command>`")
-        return
-    msg = await event.reply("Executing `{}`...".format(cmd[1]))
-    ps = await asyncio.create_subprocess_shell(cmd[1], stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    stdout, stderr = await ps.communicate()
-    output = "**Command:**\n`{}`\n\n".format(cmd[1])
-    if stdout:
-        output += "**stdout:**\n`{}`\n\n".format(stdout.decode().strip())
-    if stderr:
-        output += "**stderr**:\n`{}`".format(stderr.decode().strip())
-    if len(output) > 4096:
-        await msg.edit("Output too long, sending as file...")
-        output_ = output.replace("`", "").replace("*", "")
-        with io.BytesIO(output_.encode()) as f:
-            f.name = "output.txt"
-            await pbot.send_file(event.chat_id, file=f, reply_to=event.id)
-        await msg.delete()
-    else:
-        await msg.edit(output)
-
-@telethn.on(events.NewMessage(pattern="^/eval"))
-async def eval_action(event):
-    if not db.isAuth(event.sender_id):
-        return
-    code = event.raw_text.split(" ", 1)
-    if len(code) == 1:
-        await event.reply("Usage: `/eval <code>`")
-        return
-    msg = await event.reply("Running...")
+@app.on_message(
+    filters.command("eval")
+     & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def executor(client, message):
+    if len(message.command) < 2:
+        return await edit_or_reply(
+            message, text="__Nigga Give me some command to execute.__"
+        )
+    try:
+        cmd = message.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        return await message.delete()
+    t1 = time()
     old_stderr = sys.stderr
     old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
     try:
-        value = await aexec(code[1], event)
+        await aexec(cmd, client, message)
     except Exception:
         exc = traceback.format_exc()
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-    evaluation = exc or stderr or stdout or value or "No output"
-    output = "**Code:**\n`{}`\n\n**Output:**\n`{}`".format(code[1], evaluation)
-    if len(output) > 4096:
-        await msg.edit("Output too long, sending as file...")
-        output_ = output.replace("`", "").replace("*", "")
-        with io.BytesIO(output_.encode()) as f:
-            f.name = "output.txt"
-            await pbot.send_file(event.chat_id, file=f, reply_to=event.id)
-        await msg.delete()
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
     else:
-        await msg.edit(output)
+        evaluation = "Success"
+    final_output = f"**OUTPUT**:\n```{evaluation.strip()}```"
+    if len(final_output) > 4096:
+        filename = "output.txt"
+        with open(filename, "w+", encoding="utf8") as out_file:
+            out_file.write(str(evaluation.strip()))
+        t2 = time()
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="⏳",
+                        callback_data=f"runtime {t2-t1} Seconds",
+                    )
+                ]
+            ]
+        )
+        await message.reply_document(
+            document=filename,
+            caption=f"**INPUT:**\n`{cmd[0:980]}`\n\n**OUTPUT:**\n`Attached Document`",
+            quote=False,
+            reply_markup=keyboard,
+        )
+        await message.delete()
+        os.remove(filename)
+    else:
+        t2 = time()
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="⏳",
+                        callback_data=f"runtime {round(t2-t1, 3)} Seconds",
+                    ),
+                    InlineKeyboardButton(
+                        text="🗑",
+                        callback_data=f"forceclose abc|{message.from_user.id}",
+                    ),
+                ]
+            ]
+        )
+        await edit_or_reply(
+            message, text=final_output, reply_markup=keyboard
+        )
 
+
+
+
+
+
+
+
+
+@app.on_message(
+    filters.command("sh")
+    & ~filters.forwarded
+    & ~filters.via_bot
+)
+async def shellrunner(client, message):
+    if len(message.command) < 2:
+        return await edit_or_reply(
+            message, text="**Usage:**\n/sh git pull"
+        )
+    text = message.text.split(None, 1)[1]
+    if "\n" in text:
+        code = text.split("\n")
+        output = ""
+        for x in code:
+            shell = re.split(
+                """ (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", x
+            )
+            try:
+                process = subprocess.Popen(
+                    shell,
+                    stdout=subprocess.PIPE,
+                    stderr=subprocess.PIPE,
+                )
+            except Exception as err:
+                print(err)
+                await edit_or_reply(
+                    message, text=f"**ERROR:**\n```{err}```"
+                )
+            output += f"**{code}**\n"
+            output += process.stdout.read()[:-1].decode("utf-8")
+            output += "\n"
+    else:
+        shell = re.split(""" (?=(?:[^'"]|'[^']*'|"[^"]*")*$)""", text)
+        for a in range(len(shell)):
+            shell[a] = shell[a].replace('"', "")
+        try:
+            process = subprocess.Popen(
+                shell,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+        except Exception as err:
+            print(err)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type,
+                value=exc_obj,
+                tb=exc_tb,
+            )
+            return await edit_or_reply(
+                message, text=f"**ERROR:**\n```{''.join(errors)}```"
+            )
+        output = process.stdout.read()[:-1].decode("utf-8")
+    if str(output) == "\n":
+        output = None
+    if output:
+        if len(output) > 4096:
+            with open("output.txt", "w+") as file:
+                file.write(output)
+            await client.send_document(
+                message.chat.id,
+                "output.txt",
+                reply_to_message_id=message.message_id,
+                caption="`Output`",
+            )
+            return os.remove("output.txt")
+        await edit_or_reply(
+            message, text=f"**OUTPUT:**\n```{output}```"
+        )
+    else:
+        await edit_or_reply(message, text="**OUTPUT: **\n`No output`")
