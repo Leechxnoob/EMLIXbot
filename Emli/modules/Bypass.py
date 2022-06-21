a19ded0eeec251eaaed36b8050a69fde34e4db7b
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-06-21 13:31:53 +0530

    Create Bypass.py

---
+++ Emli/modules/Bypass.py
@@ -0,0 +1,50 @@
+import time
+
+import re
+from random import choice
+import cloudscraper
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from Emli import pbot
+from pyrogram import Client, filters
+from pyrogram.types import Update
+
+
+# eg: https://gplinks.co/XXXX
+url = ""
+
+# =======================================
+@pbot.on_message(filters.command("bypass"))
+def gplinks_bypass(url: str):
+    client = cloudscraper.create_scraper(allow_brotli=False)
+    p = urlparse(url)
+    final_url = f'{p.scheme}://{p.netloc}/links/go'
+
+    res = client.head(url)
+    header_loc = res.headers['location']
+    param = header_loc.split('postid=')[-1]
+    req_url = f'{p.scheme}://{p.netloc}/{param}'
+
+    p = urlparse(header_loc)
+    ref_url = f'{p.scheme}://{p.netloc}/'
+
+    h = { 'referer': ref_url }
+    res = client.get(req_url, headers=h, allow_redirects=False)
+
+    bs4 = BeautifulSoup(res.content, 'html.parser')
+    inputs = bs4.find_all('input')
+    data = { input.get('name'): input.get('value') for input in inputs }
+
+    h = {
+        'referer': ref_url,
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    time.sleep(10)
+    res = client.post(final_url, headers=h, data=data)
+    try:
+        return res.json()['url'].replace('\/','/')
+    except: return 'Something went wrong :('
+
+# =======================================
+
+print(gplinks_bypass(url))
