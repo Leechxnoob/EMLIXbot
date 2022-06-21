b148fbf70e572ca9550823a889a38de6504a8ba0
Leechx <98255001+Leechxnoob@users.noreply.github.com>
2022-06-21 14:14:53 +0530

    Create Callender.py

---
+++ Emli/modules/Callender.py
@@ -0,0 +1,29 @@
+import calendar  # pylint: disable=W0406
+from datetime import datetime
+
+from pyrogram import Message
+from Emli import pbot
+
+
+@app.on_message(filters.command("callender"))
+async def _calendar(message: Message):):
+    if message.reply_to_message
+     if not message.input_str:
+        await message.edit("`Searching...`")
+        try:
+            today = datetime.today()
+            input_ = calendar.month(today.year, today.month)
+            await message.edit(f"```{input_}```")
+        except Exception as e:
+            await message.err(e)
+        return
+    if '|' not in message.input_str:
+        await message.err("both year and month required!")
+        return
+    await message.edit("`Searching...`")
+    year, month = message.input_str.split('|', maxsplit=1)
+    try:
+        input_ = calendar.month(int(year.strip()), int(month.strip()))
+        await message.edit(f"```{input_}```")
+    except Exception as e:
+        await message.err(e)
